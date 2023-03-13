e8e16325adc51641264be0a2543e1cc183b44cc7
SonJava10Rikkei <sonjapan7@gmail.com>
2023-03-13 16:55:29 +0700

    Slide done cuoi

--- Demo/src/Overloading/Summary.java
+++ Demo/src/Overloading/Summary.java
@@ -4,11 +4,9 @@ public class Summary {
     static int sum(int a, int b) {
         return a + b;
     }
-
     static int sum(int a, int b, int c) {
         return a + b + c;
     }
-
     static double sum(double a, double b) {
         return a + b;
     }
--- Demo/src/Overloading/Test.java
+++ Demo/src/Overloading/Test.java
@@ -5,6 +5,5 @@ public class Test {
         System.out.println(Summary.sum(1,2));
         System.out.println(Summary.sum(1,2,3));
         System.out.println(Summary.sum(1.2,1.3));
-
     }
 }
